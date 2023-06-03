68578cbb5279cbdebe7189bfe03ac0c33bc2ac09
Philos <i@philosyang.com>
2023-06-02 23:05:11 -0500

    better listing formatting

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def itemSearchDictFormat(dic):
     return out
 
 
-def itemInfo(item_id, listings_count=20, entries=5, noGst=1):
+def itemInfo(item_id, listings_count=20, entries=5, noGst=0):
     if world_search:
         x = requests.get(
             'https://universalis.app/api/v2/' + world + '/' + item_id + '?listings=' + str(listings_count) + '&entries=' + str(entries) + '&noGst=' + str(noGst))
@@ -72,52 +72,55 @@ def itemInfoDictFormat(y):
     out = ''
 
     listings = y['listings']
-    recentHistory = y['recentHistory']
+    # recentHistory = y['recentHistory']
 
-    listingAveragePrice = str(y['currentAveragePrice']).replace('.', '\.')
-    listingAveragePriceHQ = str(y['currentAveragePriceHQ']).replace('.', '\.')
-    salesAveragePrice = str(y['averagePrice']).replace('.', '\.')
-    salesAveragePriceHQ = str(y['averagePriceHQ']).replace('.', '\.')
-    historyMinPrice = str(y['minPrice'])
-    historyMinPriceHQ = str(y['minPriceHQ'])
-    historyMaxPrice = str(y['maxPrice'])
-    historyMaxPriceHQ = str(y['maxPriceHQ'])
+    # listingAveragePrice = str(y['currentAveragePrice']).replace('.', '\.')
+    # listingAveragePriceHQ = str(y['currentAveragePriceHQ']).replace('.', '\.')
+    # salesAveragePrice = str(y['averagePrice']).replace('.', '\.')
+    # salesAveragePriceHQ = str(y['averagePriceHQ']).replace('.', '\.')
+    # historyMinPrice = str(y['minPrice'])
+    # historyMinPriceHQ = str(y['minPriceHQ'])
+    # historyMaxPrice = str(y['maxPrice'])
+    # historyMaxPriceHQ = str(y['maxPriceHQ'])
 
-    out += '_listings:_\n'
-    out += '_avg: ' + listingAveragePrice + '_\n'
-    out += '_avg \(HQ\): ' + listingAveragePriceHQ + '_\n'
-    out += '\n'
+    # out += '_listings:_\n'
+    # out += '_avg: ' + listingAveragePrice + '_\n'
+    # out += '_avg \(HQ\): ' + listingAveragePriceHQ + '_\n'
+    # out += '\n'
 
-    out += 'sales:\n'
-    out += 'avg: ' + salesAveragePrice + '\n'
-    out += 'avg \(HQ\): ' + salesAveragePriceHQ + '\n'
-    out += '\n'
+    # out += 'sales:\n'
+    # out += 'avg: ' + salesAveragePrice + '\n'
+    # out += 'avg \(HQ\): ' + salesAveragePriceHQ + '\n'
+    # out += '\n'
 
-    out += 'historical:\n'
-    out += 'min: ' + historyMinPrice + '\n'
-    out += 'min \(HQ\): ' + historyMinPriceHQ + '\n'
-    out += 'max: ' + historyMaxPrice + '\n'
-    out += 'max \(HQ\): ' + historyMaxPriceHQ + '\n'
-    out += '\n'
+    # out += 'historical:\n'
+    # out += 'min: ' + historyMinPrice + '\n'
+    # out += 'min \(HQ\): ' + historyMinPriceHQ + '\n'
+    # out += 'max: ' + historyMaxPrice + '\n'
+    # out += 'max \(HQ\): ' + historyMaxPriceHQ + '\n'
+    # out += '\n'
 
-    out += 'listings:\n'
+    # out += 'listings:\n'
     for i in range(len(listings)):
-        
+
         if listings[i]['worldName'] == world:
-            out += '🏠'
-        else:
-            out += listings[i]['worldName'][0] + ' '
+            out += '*'
+
+        out += listings[i]['worldName'][0] + ' '
 
         if listings[i]['hq']:
             out += '__'
 
-        out += str(listings[i]['pricePerUnit']) + ' x' + str(listings[i]['quantity'])
+        out += str(listings[i]['pricePerUnit']) + \
+            ' x' + str(listings[i]['quantity'])
 
         if listings[i]['hq']:
             out += '__'
+
+        if listings[i]['worldName'] == world:
+            out += '*'
 
         out += '\n'
-
 
     return out
 

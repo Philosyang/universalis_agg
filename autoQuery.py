7d56d186c21df5fe61234cda19e15ebadcfb519e
Philos <philosyang@gmail.com>
2023-06-03 01:54:52 -0500

    cert issue note

--- autoQuery.py
+++ autoQuery.py
@@ -1,6 +1,9 @@
 # you can run this with a script, then use task scheduler / cron job for a set interval (e.g., 1 hour) and your DB will be constantly fed with fresh data
 # add/remove item IDs to/from autoQueryList (100 items max)
 
+# possible cert issue:
+# https://stackoverflow.com/questions/69397039/pymongo-ssl-certificate-verify-failed-certificate-has-expired-on-mongo-atlas
+
 import requests
 import json
 
